e0a6a8d7527dee29dcb41c73fd30eed119832833
Forsythe <forsythe.dev@gmail.com>
2024-10-24 23:34:16 +0400

    Add MAIN_STACK skills constant & upload icons

---
+++ src/constants/skills.ts
@@ -0,0 +1,23 @@
+interface ISkillItem {
+  title: string
+  imageUrl: string
+}
+
+export const MAIN_STACK: ISkillItem[] = [
+  {
+    title: 'TypeScript',
+    imageUrl: '/icons/ts-icon.svg',
+  },
+  {
+    title: 'JavaScript',
+    imageUrl: '/icons/js-icon.svg',
+  },
+  {
+    title: 'React',
+    imageUrl: '/icons/react-icon.svg',
+  },
+  {
+    title: 'NodeJS',
+    imageUrl: '/icons/node-icon.svg',
+  },
+]
