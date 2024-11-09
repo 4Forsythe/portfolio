00bbff61e0e84fdacc93f0f5aee183f8142634a4
Forsythe <forsythe.dev@gmail.com>
2024-11-09 08:11:32 +0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Inter, Bebas_Neue } from 'next/font/google'
+
+import { cn } from '@/lib/utils'
+
+import './globals.css'
+
+const inter = Inter({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  variable: '--font-inter',
+})
+
+const bebasNeue = Bebas_Neue({
+  weight: ['400'],
+  style: ['normal'],
+  variable: '--font-bebas-neue',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ru">
+      <body className={cn('antialiased', inter.variable, bebasNeue.variable)}>{children}</body>
+    </html>
+  )
+}
